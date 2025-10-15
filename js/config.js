0d20259338c6aecd40efd79b503f6ec5d3147041
tsukamoto <masahiro-tsukamoto@telema.jp>
2025-10-15 10:53:10 +0900

    new config

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-northeast-1_DcVvHERAY', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '47jfsmnpmp9ehskhu6a066spk2', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '1j578qb4v5fckjilqs08626bnmo8lmvlu0i4flkt15un9o9cmlpe', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-northeast-1' // e.g. us-east-2
     },
     api: {
