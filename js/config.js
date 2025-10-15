0730494964e0526087af1d28bfb72176874055d8
tsukamoto <masahiro-tsukamoto@telema.jp>
2025-10-15 09:52:07 +0900

    new config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-northeast-1_DcVvHERAY', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '47jfsmnpmp9ehskhu6a066spk2', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-northeast-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
